37e434e6991023584720c9036a7256c6923b1f41
Robert <snaipertist@gmail.com>
2023-04-07 20:00:56 +0400

    add beeping sound

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from something.key_value import DictSearch
+from playsound import playsound
+from key_value import DictSearch
 
 morse_code_alphabet = {".-": "a", "-...": "b", "-.-.": "c", "-..": "d", ".": "e", "..-.": "f", "--.": "g", "....": "h",
                        "..": "i", ".---": "j", "-.-": "k", ".-..": "l", "--": "m", "-.": "n", "---": "o", ".--.": "p",
@@ -9,12 +10,12 @@ morse_code_alphabet = {".-": "a", "-...": "b", "-.-.": "c", "-..": "d", ".": "e"
 # to search for a key by its value
 search = DictSearch(my_dict=morse_code_alphabet)
 
-# Users input
+# # Users input
 search_value = input("Enter ").lower()
-
+#
 # Break a string into individual letters
 letters = [letter for letter in search_value]
-print(letters)
+# print(letters)
 
 empty = []
 
@@ -23,5 +24,19 @@ for letter in letters:
     key = search.get_key_by_value(search_value=letter)
     empty.append(key)
 
+
+for message in empty:
+    sound_letters = [sound_letter for sound_letter in message]
+    print(sound_letters)
+    if "." in sound_letters:
+        for i in range(len(sound_letters)):
+            print("True")
+            playsound("sounds/dot_morse_code1.mp3")
+    if "-" in sound_letters:
+        for i in range(len(sound_letters)):
+            print("False")
+            playsound("sounds/line_morse_code1.mp3")
 final_code = " ".join(empty)
 print(final_code)
+
+# playsound("sounds/b_morse_code1.mp3")
